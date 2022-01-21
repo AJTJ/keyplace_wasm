d3716c9e34444982ec091fe447f84757b1a0d01f
AJTJ <janke.aaron@gmail.com>
2022-01-21 12:56:36 -0700

    updated to use server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// FOR TARGET WEB
+// // FOR TARGET WEB WITH SERVER
 import init, { get_all_keys, get_agent_key } from "./pkg/keyplace_wasm.js";
 const runWasm = async () => {
   const pw = "dank memes";
@@ -10,7 +10,23 @@ const runWasm = async () => {
 
   console.log("WEB - get_all_keys result", allKeysJson.agent_key.keypair);
 
-  const custKey = allKeysJson.custodial_key;
+  let save_data = {
+    email,
+    custodial_key: allKeysJson.custodial_key,
+  };
+
+  let saveKeyResponse = await fetch("http://127.0.0.1:8080/save_key", {
+    method: "POST",
+    body: JSON.stringify(save_data),
+  });
+
+  let getKeyResponse = await fetch("http://127.0.0.1:8080/get_key", {
+    method: "POST",
+    body: JSON.stringify({ email }),
+  });
+
+  let getKeyJson = await getKeyResponse.json();
+  let custKey = getKeyJson.custodial_key;
 
   const agentKeyResult = await get_agent_key(JSON.stringify(custKey), pw);
   const agentKeyJson = JSON.parse(agentKeyResult);
@@ -18,6 +34,28 @@ const runWasm = async () => {
   console.log("WEB - get_agent_key result", agentKeyJson.agent_key.keypair);
 };
 runWasm();
+
+// WITH LOCAL PACKAGE
+// FOR TARGET WEB
+// import init, { get_all_keys, get_agent_key } from "./pkg/keyplace_wasm.js";
+// const runWasm = async () => {
+//   const pw = "dank memes";
+//   const email = "yes@yes.com";
+
+//   await init();
+//   const allKeysResult = await get_all_keys(email, pw);
+//   const allKeysJson = JSON.parse(allKeysResult);
+
+//   console.log("WEB - get_all_keys result", allKeysJson.agent_key.keypair);
+
+//   const custKey = allKeysJson.custodial_key;
+
+//   const agentKeyResult = await get_agent_key(JSON.stringify(custKey), pw);
+//   const agentKeyJson = JSON.parse(agentKeyResult);
+
+//   console.log("WEB - get_agent_key result", agentKeyJson.agent_key.keypair);
+// };
+// runWasm();
 
 // OUTDATED
 // FOR TARGET NODE
